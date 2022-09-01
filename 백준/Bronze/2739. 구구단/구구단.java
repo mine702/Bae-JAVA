75ecf223657397df00769b39594af7d345125bdd
mine702 <107476541+mine702@users.noreply.github.com>
2022-09-01 17:14:43 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18556 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        scanner.close();
+        for (int i = 1; i < 10; i++) {
+            System.out.println(A + " * " + i + " = " + A * i);
+        }
+    }
+}
