f45876e16daa7cfcd39ff39f4c4673d5077d80c8
mine702 <107476541+mine702@users.noreply.github.com>
2022-10-12 15:11:00 +0900

    [Bronze IV] Title: 과자, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int sum = K * N - M;
+        if (sum <= 0) {
+            bw.write("0");
+        } else {
+            bw.write(String.valueOf(sum));
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
