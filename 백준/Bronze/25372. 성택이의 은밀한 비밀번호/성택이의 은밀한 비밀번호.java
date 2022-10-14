38bbbd91092dc49e57519e9edacf721b51ce4c72
mine702 <107476541+mine702@users.noreply.github.com>
2022-10-14 10:29:04 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 128 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String password = br.readLine();
+            if (password.length() >= 6 && password.length() <= 9) {
+                bw.write("yes\n");
+            } else {
+                bw.write("no\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
