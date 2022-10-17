1c325061455e0eba0f41fea49f6eb4b768423ae8
mine702 <107476541+mine702@users.noreply.github.com>
2022-10-17 15:36:15 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
