80f48294b44f53c14a42fc214b0bae7b00d6c923
mine702 <107476541+mine702@users.noreply.github.com>
2022-10-19 17:27:54 +0900

    [Bronze V] Title: 새로운 시작, Time: 116 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("파이팅!!\n");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
