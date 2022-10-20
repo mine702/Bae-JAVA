79f799dd510c9bcedb4e952c70eb0c43f206426f
mine702 <107476541+mine702@users.noreply.github.com>
2022-10-20 11:24:10 +0900

    [Bronze V] Title: 나는 누구인가, Time: 124 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("I'm Sexy\n");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
