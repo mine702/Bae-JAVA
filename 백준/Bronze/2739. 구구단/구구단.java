22495b94fb4c0b3d9ad2acfcccaf183e4253ebf8
mine702 <107476541+mine702@users.noreply.github.com>
2022-11-08 16:40:13 +0900

    [Bronze V] Title: 구구단, Time: 164 ms, Memory: 16372 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,12 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        scanner.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
         for (int i = 1; i < 10; i++) {
-            System.out.println(A + " * " + i + " = " + A * i);
+            bw.write(String.valueOf(n) + " * " + String.valueOf(i) + " = " + String.valueOf(n * i) + "\n");
         }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
