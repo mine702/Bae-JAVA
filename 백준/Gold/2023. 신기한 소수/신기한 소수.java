3c24819ddcf65d45cb92c4ff71a015e39ebf6159
mine702 <107476541+mine702@users.noreply.github.com>
2023-05-19 17:06:00 +0900

    [Gold V] Title: 신기한 소수, Time: 140 ms, Memory: 16060 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -37,17 +37,17 @@ public class Main
 		}
 	}
 	
-	public static Boolean isPrime(int num) {
-		if(num < 2) {
-			return false;
-		}else {
-			for(int i = 2; i < num/2; i++) {
-				if(num % i == 0) {
-					return false;
-				}
-			}
-			return true;
-		}
+	public static boolean isPrime(int num) {
+	    if (num < 2) {
+	        return false;
+	    }
+	    for (int i = 2; i * i <= num; i++) {
+	        if (num % i == 0) {
+	            return false;
+	        }
+	    }
+	    return true;
 	}
+
 }
 
