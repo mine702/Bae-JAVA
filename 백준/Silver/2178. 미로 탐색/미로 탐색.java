f7faf5aa622ca35c902485ffc7fb31ae446d2015
mine702 <107476541+mine702@users.noreply.github.com>
2023-05-20 15:32:16 +0900

    [Silver I] Title: 미로 탐색, Time: 152 ms, Memory: 15484 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,46 +3,53 @@ import java.util.*;
 
 public class Main
 {
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {-1,1,0,0};
-	static int[][] arrs1;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	
+	static int[][] room;
 	static boolean[][] bl;
-	static int N,M;
-	public static void main(String args[])throws Exception
-	{
+	static StringBuilder sb = new StringBuilder();
+	static int N;
+	static int M;
+	public static void main(String args[])throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+		
 		st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		arrs1 = new int[N][M];
+		
+		room = new int[N][M];
 		bl = new boolean[N][M];
-		for(int i = 0; i < N ; i++) {
+		for(int i = 0; i < N; i++) {
 			String line = br.readLine();
-			for(int j = 0 ; j < M ; j++) {
-				arrs1[i][j] = Integer.parseInt(line.substring(j, j+1));
+			for(int j = 0; j < M; j++) {
+				int num = Integer.parseInt(line.substring(j, j+1));
+				room[i][j] = num;
 			}
 		}
 		BFS(0,0);
-		sb.append(arrs1[N-1][M-1]);
+		sb.append(room[N-1][M-1]);
 		System.out.println(sb);
 	}
 	
-	public static void BFS(int i , int j) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] {i,j});
-		bl[i][j] = true;
-		while(!queue.isEmpty()) {
-			int now[] = queue.poll();
-			for(int k = 0; k < 4; k++) {
-				int x = now[0] + dx[k];
-				int y = now[1] + dy[k];
-				if(x >= 0 && y >= 0 && x < N && y < M) {
-					if(arrs1[x][y] != 0 && !bl[x][y]) {
-						bl[x][y] = true;
-						arrs1[x][y] = arrs1[now[0]][now[1]] + 1;
-						queue.add(new int[] {x,y});
+	public static void BFS(int x, int y) {
+		Queue<int[]> q1 = new LinkedList<>();
+		int[] start = {x, y};
+		q1.add( start );
+		bl[x][y] = true;
+		while(!q1.isEmpty()) {
+			int[] now = q1.poll();
+			for(int i = 0; i < 4; i++) {
+				int n1 = now[0] + dx[i];
+				int n2 = now[1] + dy[i];
+				if( n1 >= 0 && n2 >= 0 && n1 < N && n2 < M) {
+					if(bl[n1][n2] == false && room[n1][n2] != 0) {						
+						int[] go = {n1 , n2};
+						q1.add(go);
+						bl[n1][n2] = true;
+						room[n1][n2] += room[now[0]][now[1]];
 					}
 				}
 			}
