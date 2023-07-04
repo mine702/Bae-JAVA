15ba5ef5721f4b7b97677246785940fbef9d080a
mine702 <107476541+mine702@users.noreply.github.com>
2023-07-04 17:12:33 +0900

    [D1] Title: 최대수 구하기, Time: 105 ms, Memory: 17,424 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -1,28 +1,23 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i < T + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int max = 0;
+            for (int j = 0; j < 10; j++) {
+                int a = Integer.parseInt(st.nextToken());
+                if(max < a){
+                    max = a;
+                }
+            }
+            sb.append("#" + i + " " + max + "\n");
+        }
 
-public class Solution
-{
-	public static void main(String args[])throws Exception
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st ;
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		for(int i = 1;i<=n;i++) {
-			int result = 0;
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0;j<10;j++) {
-				int num = Integer.parseInt(st.nextToken());
-				if(result < num) {
-					result = num;
-				}
-			}
-			sb.append("#"+i+" "+result+"\n");
-		}		
-		br.close();	
-		System.out.println(sb);
-	}
+        System.out.println(sb);
+    }
 }
