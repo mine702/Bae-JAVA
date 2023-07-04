c26d59ada851675beb73e51fa60e6246ed171da0
mine702 <107476541+mine702@users.noreply.github.com>
2023-07-04 17:35:15 +0900

    [D1] Title: 중간값 찾기, Time: 102 ms, Memory: 18,876 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -1,25 +1,19 @@
-import java.io.*;
 import java.util.*;
-
-public class Solution
-{
-	public static void main(String args[])throws Exception
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st ;
-		
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		int result = 0;
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n ;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}		
-		Arrays.sort(arr);
-		double m = Math.round(n/2);
-		sb.append(arr[(int)m]);
-		br.close();	
-		System.out.println(sb);
-	}
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> al = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            al.add(num);
+        }
+        Collections.sort(al);
+        sb.append(al.get( N / 2 ));
+        System.out.println(sb);
+    }
 }
