234ba6489b23b170daf3e1a1988f0c8d8bd61ae5
mine702 <107476541+mine702@users.noreply.github.com>
2023-07-21 19:22:01 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		int count = 1; // 겹 수(최소 루트)
-		int range = 2;	// 범위 (최솟값 기준) 
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int count = 1; 
+		int range = 2;	
  
 		if (N == 1) {
-			System.out.print(1);
+			sb.append(1);
 		}
  
 		else {
-			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
-				range = range + (6 * count);	// 다음 범위의 최솟값으로 초기화 
-				count++;	// count 1 증가 
+			while (range <= N) {	
+				range = range + (6 * count);	 
+				count++;	
 			}
-			System.out.print(count);
+			sb.append(count);
 		}
-	}
+		System.out.println(sb);
+	}	
 }
