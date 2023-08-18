abb3b1d54ee48caf7bb28f135b7c9ab6eb33855f
mine702 <107476541+mine702@users.noreply.github.com>
2023-08-18 14:34:30 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,42 +1,32 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	public static int N, r, c, count = 0;
+	public static int N, r, c;
+	public static int cnt = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
-		search(0, 0, (int) Math.pow(2, N));
-		sb.append(count);
+
+		zCur(0, 0, (int) Math.pow(2, N));
+		sb.append(cnt);
 		System.out.println(sb);
 	}
 
-	private static void search(int startY, int startX, int size) {
-		if (size == 1) {
+	private static void zCur(int startX, int startY, int size) {
+		if (size <= 1)
 			return;
-		}
-		
-		int newSize = size / 2;
-		int midY = startY + newSize;
-		int midX = startX + newSize;
-		
-		if (r < midY && c < midX) {
-			search(startY, startX, newSize);
-		} else if (r < midY && c >= midX) {
-			count += newSize * newSize;
-			search(startY, midX, newSize);
-		} else if (r >= midY && c < midX) {
-			count += 2 * newSize * newSize;
-			search(midY, startX, newSize);
-		} else {
-			count += 3 * newSize * newSize;
-			search(midY, midX, newSize);
-		}
+		int xOffset = (r - startX) / (size / 2);
+		int yOffset = (c - startY) / (size / 2);
+		cnt += (int) Math.pow(size / 2, 2) * (xOffset * 2 + yOffset);
+		zCur(startX + (size / 2) * xOffset, startY + (size / 2) * yOffset, size / 2);
 	}
+
 }
