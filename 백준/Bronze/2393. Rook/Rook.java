f58322d0e9aa90bd2ef8b76934b88c2beb84dba5
mine702 <107476541+mine702@users.noreply.github.com>
2023-09-06 20:46:54 +0900

    [Bronze V] Title: Rook, Time: 120 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		sb.append("  ___  ___  ___\n");
+		sb.append("  | |__| |__| |\n");
+		sb.append("  |           |\n");
+		sb.append("   \\_________/\n");
+		sb.append("    \\_______/\n");
+		sb.append("     |     |\n");
+		sb.append("     |     |\n");
+		sb.append("     |     |\n");
+		sb.append("     |     |\n");
+		sb.append("     |_____|\n");
+		sb.append("  __/       \\__\n");
+		sb.append(" /             \\\n");
+		sb.append("/_______________\\");
+		System.out.println(sb);
+
+	}
+}
