b823b9669c906fd03b128e26c0315e58c34eeb08
mine702 <107476541+mine702@users.noreply.github.com>
2023-09-13 09:20:59 +0900

    [unrated] Title: 미로 탈출 명령어, Time: 0.54 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,42 +1,72 @@
+import java.util.*;
 class Solution {
-    private static final int[] dx = { 1, 0, 0, -1};
-    private static final int[] dy = { 0, -1, 1, 0};
-    private static final String[] term = {"d", "l", "r", "u"};
-    private static int mapX, mapY;
-    private static int endX, endY;
-    private String tempAnswer = "";
+    public static class Node implements Comparable<Node> {
+		int x;
+		int y;
+		int time;
+		String route;
 
-    public boolean dfs(int x, int y, int k, String str, int diff) {
-        if(k==0 && diff==0){
-            tempAnswer = str;
-            return true;
-        }
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-            if(nextX >=0 && nextY >= 0 && nextX < mapX && nextY < mapY && diff<=k) {
-                if ((diff % 2 == 0 && k % 2 ==0) || (diff % 2 == 1 && k % 2 ==1)) {
-                    if (dfs(nextX, nextY, k - 1, str + term[i], Math.abs(nextX - endX) + Math.abs(nextY - endY))) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-    public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        String answer;
-        mapX = n;
-        mapY = m;
-        endX = r-1;
-        endY = c-1;
-        int diff = Math.abs((r-1)-(x-1)) + Math.abs((c-1)-(y-1));
-        dfs(x-1, y-1, k, "", diff);
-        if(tempAnswer.equals("")){
-            answer = "impossible";
-        } else {
-            answer = tempAnswer;
-        }
-        return answer;
-    }
+		Node(int x, int y, int time, String route) {
+			this.x = x;
+			this.y = y;
+			this.time = time;
+			this.route = route;
+		}
+
+		public String toString() {
+			return route;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return this.route.compareTo(o.route);
+		}
+	}
+
+	static int[] dx = { 1, 0, 0, -1 }, dy = { 0, -1, 1, 0 };
+
+	public static int[][] locations = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	
+	public String solution(int n, int m, int x, int y, int r, int c, int k) {
+		PriorityQueue<Node> q = new PriorityQueue<>();
+		q.offer(new Node(x - 1, y - 1, 0, ""));
+		boolean[][] v = new boolean[n][m];
+		v[x - 1][y - 1] = true;
+		while (!q.isEmpty()) {
+			Node now = q.poll();
+			if (now.x == r - 1 && now.y == c - 1 && now.time == k)
+				return now.route;
+			if (now.time > k)
+				continue;
+			if (!ispossible(now.x, now.y, r - 1, c - 1, k - now.time))
+				continue;
+			for (int d = 0; d < 4; d++) {
+				int nx = now.x + dx[d];
+				int ny = now.y + dy[d];
+				if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+					continue;
+				String Nroute = "";
+				if (d == 0)
+					Nroute = now.route + "d";
+				else if (d == 1)
+					Nroute = now.route + "l";
+				else if (d == 2)
+					Nroute = now.route + "r";
+				else
+					Nroute = now.route + "u";
+				q.offer(new Node(nx, ny, now.time + 1, Nroute));
+			}
+		}
+		return "impossible";
+	}
+
+	boolean ispossible(int x, int y, int r, int c, int time) {
+		int rest = Math.abs(r - x) + Math.abs(c - y);
+		if (rest > time)
+			return false;
+		if ((rest % 2) - (time % 2) == 0)
+			return true;
+		return false;
+	}
 }
