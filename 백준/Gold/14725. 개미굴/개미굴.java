7f327f188dd523ae60847bd9895785c193ba736d
mine702 <107476541+mine702@users.noreply.github.com>
2023-10-04 13:07:39 +0900

    [Gold III] Title: 개미굴, Time: 244 ms, Memory: 16904 KB -BaekjoonHub

---
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Node root = new Node();
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            int size = Integer.parseInt(st.nextToken());
+            Node cur = root;
+
+            for(int j=0; j<size; j++){
+                String s = st.nextToken();
+
+                if(!cur.childs.containsKey(s)){
+                    cur.childs.put(s, new Node());
+                }
+                cur = cur.childs.get(s);
+            }
+        }
+        print(root, "");
+    }
+
+    public static void print(Node root, String bar){
+        Object[] key = root.childs.keySet().toArray();
+        Arrays.sort(key);
+
+        for (Object s : key){
+            System.out.println(bar+s);
+            print(root.childs.get(s),bar+"--");
+        }
+    }
+
+}
+
+class Node{
+    HashMap<String, Node> childs = new HashMap<>();
+}
