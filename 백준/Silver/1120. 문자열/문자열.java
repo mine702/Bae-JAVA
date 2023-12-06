1c1fe65d7ed1b243bfe5ffd6f2cd1a207acd48c0
mine702 <107476541+mine702@users.noreply.github.com>
2023-12-07 00:05:44 +0900

    [Silver IV] Title: 문자열, Time: 200 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.next();
+        String b = scanner.next();
+
+        int result = a.length();
+
+        for(int i=0;i<b.length()-a.length()+1;i++){
+            int tmp=0;
+
+            for(int j=0;j<a.length();j++){
+                if(a.charAt(j)!=b.charAt(j+i)){
+                    tmp++;
+                }
+            }
+            if(result > tmp){
+                result = tmp;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
