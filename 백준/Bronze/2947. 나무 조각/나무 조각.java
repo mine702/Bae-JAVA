0cd692c56735ec7d07b7b8e35015875053f4b92e
mine702 <107476541+mine702@users.noreply.github.com>
2023-12-11 17:36:40 +0900

    [Bronze I] Title: 나무 조각, Time: 216 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[5];
+        for (int i = 0; i < 5; i++) {
+            nums[i] = sc.nextInt();
+        }
+        for (int j=0;j<5;j++){
+            for (int i = 0; i < 4; i++) {
+                if(nums[i]>nums[i+1]){
+                    int tmp = nums[i];
+                    nums[i] = nums[i+1];
+                    nums[i+1] = tmp;
+                    for (int num : nums) {
+                        System.out.print(num+" ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
