5a0e519704f64e0e4071dbf0c22d21e4d37e9a17
mine702 <107476541+mine702@users.noreply.github.com>
2024-05-07 23:33:06 +0900

    [Bronze III] Title: 신기한 수, Time: 416 ms, Memory: 17912 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.java
+++ 백준/Bronze/17618. 신기한 수/신기한 수.java
@@ -2,23 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        int num= sc.nextInt();
+        int count=0;
 
-        for( int i = 1; i <= n; i++ ){
-            String str = Integer.toString(i);
-            String[] arr = str.split(""); 
-            int sum = 0;
-            for( int j = 0; j < arr.length; j++ ){
-                sum+=Integer.parseInt(arr[j]); 
+        for(int i=1;i<=num;i++){
+            int sum =0;
+            int n=i;
+
+            while(n!=0){
+                sum+=n%10; 
+                n=n/10; 
             }
 
-            if( i % sum == 0 ) count++; 
+            if(i%sum==0) count++;
+
         }
-
         System.out.println(count);
-
     }
-
 }
