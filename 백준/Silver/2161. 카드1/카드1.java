cf648901e777226c2e478f4e35cf3acb5eee1160
mine702 <107476541+mine702@users.noreply.github.com>
2024-11-16 22:31:02 +0900

    [Silver V] Title: 카드1, Time: 220 ms, Memory: 18752 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[]args){
+
+
+        Scanner scan=new Scanner(System.in);
+
+        int n= scan.nextInt();
+
+        Queue<Integer> que=new LinkedList();
+
+        for(int i=1;i<=n;i++){
+            que.add(i);
+        }
+        while(que.size()!=1){
+            int s=que.poll();
+            System.out.print(s+" ");
+            int qu=que.poll();
+            que.add(qu);
+        }
+        System.out.print(que.poll());
+
+
+    }
+}
