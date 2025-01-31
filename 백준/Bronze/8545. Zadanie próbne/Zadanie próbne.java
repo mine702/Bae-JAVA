3a4bd868b7a2ec576436684f2a519a80c2d16886
김민건 <107476541+mine702@users.noreply.github.com>
2025-01-31 22:56:37 +0900

    [Bronze V] Title: Zadanie próbne, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(new StringBuilder(br.readLine()).reverse());
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
