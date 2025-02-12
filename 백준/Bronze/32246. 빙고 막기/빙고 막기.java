d95b49036c698cb9461e039c4c5744364804bdeb
김민건 <107476541+mine702@users.noreply.github.com>
2025-02-12 23:54:20 +0900

    [Bronze III] Title: 빙고 막기, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/32246. 빙고 막기/빙고 막기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N == 2) {
+            System.out.println(3);
+        } else {
+            System.out.println(N);
+        }
+    }
+}
