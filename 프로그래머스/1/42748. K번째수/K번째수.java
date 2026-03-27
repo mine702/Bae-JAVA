c321bce02f45bd7832d67f03fbb1e2b1dc34ac97
김민건 <107476541+mine702@users.noreply.github.com>
2026-03-28 04:31:40 +0900

    [level 1] Title: K번째수, Time: 0.96 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int k = commands[i][2];
+
+            int[] numbers = Arrays.copyOfRange(array, start - 1, end);
+            Arrays.sort(numbers);
+            answer[i] = numbers[k - 1];
+        }
+
+        return answer;
+    }
+}
