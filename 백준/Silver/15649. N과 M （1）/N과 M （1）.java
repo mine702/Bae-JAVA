621f8b05a886371c7ddcf81f1442561cc7141052
김민건 <107476541+mine702@users.noreply.github.com>
2026-04-22 19:33:02 +0900

    [Silver III] Title: N과 M (1), Time: 284 ms, Memory: 33880 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    public static boolean[] isCheck;
+    public static int[] save;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        isCheck = new boolean[N];
+        save = new int[M];
+        com(0, N, M);
+        System.out.println(sb);
+    }
+
+    public static void com(int depth, int N, int R) {
+        if (depth == R) {
+            for (int i = 0; i < save.length; i++) {
+                sb.append(save[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (isCheck[i])
+                continue;
+            save[depth] = i + 1;
+            isCheck[i] = true;
+            com(depth + 1, N, R);
+            isCheck[i] = false;
+        }
+    }
+}
