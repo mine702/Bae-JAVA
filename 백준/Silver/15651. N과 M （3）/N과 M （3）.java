d3ad8e0171cbc9cfa18d81fa4a75b3fc25328bb8
김민건 <107476541+mine702@users.noreply.github.com>
2026-04-22 20:18:03 +0900

    [Silver III] Title: N과 M (3), Time: 532 ms, Memory: 207120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,40 +1,38 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Main {
-	
-	public static int[] arr;
-	public static int N, M;
-	public static StringBuilder sb = new StringBuilder();
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringTokenizer st = new StringTokenizer(br.readLine());
- 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
- 
-		arr = new int[M];
-		dfs(0);
-		System.out.println(sb);
-	}
- 
-	public static void dfs(int depth) {
- 
-		if (depth == M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(arr[i]).append(' ');
-			}
-			sb.append('\n');
-			return;
-		}
- 
-		for (int i = 1; i <= N; i++) {
-			arr[depth] = i;
-			dfs(depth + 1);
-		}
-	}
- 
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    public static boolean[] isCheck;
+    public static int[] save;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        isCheck = new boolean[N];
+        save = new int[M];
+        com(0, N, M);
+        System.out.println(sb);
+    }
+
+    public static void com(int depth, int N, int R) {
+        if (depth == R) {
+            for (int i = 0; i < save.length; i++) {
+                sb.append(save[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+
+            save[depth] = i + 1;
+            com(depth + 1, N, R);
+        }
+    }
 }
