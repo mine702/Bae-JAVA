833debb841e9b815f120b10271aea6290e4ad0be
김민건 <107476541+mine702@users.noreply.github.com>
2026-04-23 17:42:18 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,39 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            boolean flag = false;
             Stack<Character> stack = new Stack<>();
-            String bracket = br.readLine();
-            for (int j = 0; j < bracket.length(); j++) {
-                char a = bracket.charAt(j);
-                if (stack.size() == 0) {
-                    if (a == '(') {
-                        stack.push(a);
-                    } else {
-                        stack.push(a);
+            String str = br.readLine();
+
+            for (int j = 0; j < str.length(); j++) {
+                if (flag)
+                    break;
+
+                char ch = str.charAt(j);
+
+                if (ch == '(')
+                    stack.add('(');
+                else if (ch == ')') {
+                    if (stack.isEmpty()) {
+                        flag = true;
                         break;
                     }
-                } else {
-                    if (stack.peek() == a) {
-                        stack.push(a);
-                    } else {
-                        stack.pop();
-                    }
+
+                    stack.pop();
                 }
             }
-            if (stack.empty()) {
-                bw.write("YES\n");
-            } else {
-                bw.write("NO\n");
-            }
-        }
-        br.close();
-        bw.flush();
-        bw.close();
-    }
 
+            if (flag || !stack.isEmpty())
+                sb.append("NO\n");
+            else
+                sb.append("YES\n");
+        }
+        System.out.println(sb);
+    }
 }
