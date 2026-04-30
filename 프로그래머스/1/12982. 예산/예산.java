2fb6b42688ef3572b07f1bdb39968cbe7ef55dbd
김민건 <107476541+mine702@users.noreply.github.com>
2026-04-30 17:27:35 +0900

    [level 1] Title: 예산, Time: 0.55 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        int sum = 0;
+        for(int i = 0; i < d.length; i++){
+            sum += d[i];
+            if(budget >= sum)
+                answer++;
+            else
+                break;
+        }
+        
+        return answer;
+    }
+}
