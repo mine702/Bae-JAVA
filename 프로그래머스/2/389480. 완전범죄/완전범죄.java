260cee48d75d22921ce0cdb29bf295827921d843
김민건 <107476541+mine702@users.noreply.github.com>
2026-05-03 18:00:39 +0900

    [level 2] Title: 완전범죄, Time: 0.28 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -3,34 +3,35 @@ import java.util.*;
 class Solution {
     public int solution(int[][] info, int n, int m) {
         int[] dp = new int[m];
-        int INF = 1000000;
-        Arrays.fill(dp, INF);
+        
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
         dp[0] = 0;
-
-        for (int[] item : info) {
-            int aTrace = item[0];
-            int bTrace = item[1];
-
-            for (int j = m - 1; j >= 0; j--) {
-                if (dp[j] == INF) continue;
-
-                int currentA = dp[j];
-
-                if (j + bTrace < m) {
-                    dp[j + bTrace] = Math.min(dp[j + bTrace], currentA);
+        
+        for(int[] value : info){
+            int a = value[0];
+            int b = value[1];
+            
+            for(int i = m - 1; i >= 0; i--){
+                if(dp[i] == Integer.MAX_VALUE)
+                    continue;
+                
+                if(b + i < m){
+                    dp[b+i] = Math.min(dp[b+i] , dp[i]);
                 }
-
-                dp[j] = currentA + aTrace;
+                
+                dp[i] = dp[i] + a;
             }
         }
-
-        int answer = INF;
-        for (int j = 0; j < m; j++) {
-            if (dp[j] < n) {
-                answer = Math.min(answer, dp[j]);
-            }
+        
+        int answer = Integer.MAX_VALUE;
+        for(int i = 0 ; i < dp.length; i++){
+            if(dp[i] < n && dp[i] < answer)
+                answer = dp[i];
         }
-
-        return (answer == INF) ? -1 : answer;
+        
+        if(answer == Integer.MAX_VALUE)
+            answer = -1;
+        return answer;
     }
 }
