4a47f3b5e40e9ce2d472950700e1bd5af5b2a4cd
김민건 <107476541+mine702@users.noreply.github.com>
2026-05-09 06:57:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.79 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,53 +2,53 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    class Node{
-        int count = 0;
-        int y;
-        int x;
+    
+    public class Node{
+        int Y;
+        int X;
+        int count;
         
-        Node(int count, int y, int x){
+        Node(int Y, int X, int count){
+            this.Y = Y;
+            this.X = X;
             this.count = count;
-            this.y = y;
-            this.x = x;
         }
     }
     
-    public static int[][] locations = {{-1,0},{1,0},{0,-1},{0,1}};
-    public int solution(int[][] maps) {
-        int answer = 0;
-        answer = bfs(maps);
-        return answer;
-    }
+    public int[][] locations = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     
-    public int bfs(int[][] maps){
-        int min = -1; 
+    public int solution(int[][] maps) {
+        int answer = -1;
         Queue<Node> queue = new LinkedList<>();
         boolean[][] isCheck = new boolean[maps.length][maps[0].length];
-        queue.add(new Node(1, 0, 0));
+        
+        queue.add(new Node(0, 0, 1));
         isCheck[0][0] = true;
+        
         while(true){
             if(queue.isEmpty())
                 break;
             
             Node node = queue.poll();
+            if(node.Y == maps.length - 1 && node.X == maps[0].length - 1){
+                answer = node.count;
+                break;
+            }
             
-            for(int i = 0; i < locations.length; i++){
-                int y = node.y + locations[i][0];
-                int x = node.x + locations[i][1];
+            for(int i = 0 ; i < locations.length; i++){
+                int y = node.Y + locations[i][0];
+                int x = node.X + locations[i][1];
                 
-                if(0 > y || y >= maps.length || 0 > x || x >= maps[0].length || isCheck[y][x] || maps[y][x] == 0){
+                if(y < 0 || x < 0 || y >= maps.length || x >= maps[0].length)
                     continue;
-                }
+                
+                if(isCheck[y][x] || maps[y][x] == 0)
+                    continue;
                 
                 isCheck[y][x] = true;
-                if(y == maps.length - 1 && x == maps[0].length - 1){
-                    min = node.count + 1;
-                } else 
-                    queue.add(new Node(node.count + 1, y, x));
+                queue.add(new Node(y, x, node.count + 1));
             }
         }
-        
-        return min;
+        return answer;
     }
 }
